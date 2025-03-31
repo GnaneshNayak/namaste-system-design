862085c1429ca9d103a92d1196e41854a0da76ec
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2025-03-31 16:35:24 +0530

    sse

---
+++ communtication/sse/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.get('/sse', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Connection', 'keep-alive');
+  res.setHeader('Cache-Control', 'no-cache');
+
+  res.write('data: welcome to sse \n\n');
+
+  setInterval(() => {
+    res.write(`data: new seconds ${new Date().getSeconds()} \n\n`);
+  }, 1000);
+});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8000, () => {
+  console.log('Listening on port 8000');
+});
